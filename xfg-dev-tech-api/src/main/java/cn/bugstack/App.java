cba90279c22d28a5dc9bcb5c16b659596d54a976
yjz <2971921468@qq.com>
2025-12-03 16:49:25 +0800

    初始

---
+++ xfg-dev-tech-api/src/main/java/cn/bugstack/App.java
@@ -0,0 +1,13 @@
+package cn.bugstack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
