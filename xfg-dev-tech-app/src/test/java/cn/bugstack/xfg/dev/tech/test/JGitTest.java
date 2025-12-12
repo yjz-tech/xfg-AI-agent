edd56252047168a112a4569d9214185e2c45674e
yjz <2971921468@qq.com>
2025-12-12 16:59:41 +0800

    第七节: 基于AI工具,对接接口

---
+++ xfg-dev-tech-app/src/test/java/cn/bugstack/xfg/dev/tech/test/JGitTest.java
@@ -0,0 +1,15 @@
+package cn.bugstack.xfg.dev.tech.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@Slf4j
+@RunWith(SpringRunner.class)
+public class JGitTest {
+
+
+
+}
